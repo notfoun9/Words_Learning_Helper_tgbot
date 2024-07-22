67eb49f4f85057ab10b68f8fa3a5d2e383e647c6
Maksim Shishkov <patiford@mail.ru>
2024-07-22 06:11:43 +0300

    telegram-bot init commit / tgClient implemented

---
+++ clients/telegram/telegram.go
@@ -0,0 +1,93 @@
+package telegram
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/url"
+	"path"
+	"strconv"
+	"telegram-bot/lib/e"
+)
+
+const (
+	getUpdatesMethod  = "getUpdates"
+	sendMessageMethod = "sendMessage"
+)
+
+type Client struct {
+	host     string
+	basePath string
+	client   http.Client
+}
+
+func New(host string, token string) Client {
+	return Client{
+		host:     host,
+		basePath: newBasePath(token),
+		client:   http.Client{},
+	}
+}
+
+func (c *Client) Update(offset int, limit int) ([]Update, error) {
+	query := url.Values{}
+	query.Add("offset", strconv.Itoa(offset))
+	query.Add("limit", strconv.Itoa(limit))
+
+	data, err := c.doRequest(getUpdatesMethod, query)
+	if err != nil {
+		return nil, err
+	}
+
+	var res UpdateResponse
+
+	if err := json.Unmarshal(data, &res); err != nil {
+		return nil, err
+	}
+
+	return res.Result, nil
+}
+
+func (c *Client) SendMessage(chatID int, text string) error {
+	query := url.Values{}
+	query.Add("chatID", strconv.Itoa(chatID))
+	query.Add("text", text)
+
+	_, err := c.doRequest(sendMessageMethod, query)
+
+	return e.Wrap("cant send message", err)
+}
+
+func (c *Client) doRequest(method string, query url.Values) (data []byte, err error) {
+	defer func() { err = e.Wrap("can't do request", err) }()
+
+	u := url.URL{
+		Scheme: "https",
+		Host:   c.host,
+		Path:   path.Join(c.basePath, method),
+	}
+
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.URL.RawQuery = query.Encode()
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = resp.Body.Close() }()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func newBasePath(token string) string {
+	return "bot" + token
+}
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"telegram-bot/clients/telegram"
+)
+
+const (
+	tgBotHost = "api.telegram.org"
+)
+
+func main() {
+	tgCliemt := telegram.New(mustToken(), tgBotHost)
+
+	// fetcher = fetcher.New(tgCliemt)
+
+	// processot = processor.New(tgCliemt)
+	// consumer.Start(fetcher, processor)
+
+}
+
+func mustToken() string {
+	token := flag.String(
+		"telegram-bot-token",           // name
+		"",                             // value
+		"token to acsess telegram bot", //usage
+	)
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not specified")
+	}
+
+	return *token
+}
